6cc27b51ea1c087893c4ed9ea668d7861b06610c
Alibek <nurmahanovalibek@gmail.com>
2023-09-07 06:25:10 +0600

    updated

--- src/main/java/kz/dar/university/employee/core/api/repository/EmployeeEntity.java
+++ src/main/java/kz/dar/university/employee/core/api/repository/EmployeeEntity.java
@@ -1,4 +1,36 @@
 package kz.dar.university.employee.core.api.repository;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.*;
+@Entity
+@Table(name = "employee_table")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeEntity {
+    @Id
+    @Generated
+    private Long id;
+    @Column(unique = true)
+    private String employeeId;
+    @Column(nullable = false, length = 50)
+    private String name;
+    @Column(nullable = false, length = 50)
+    private String surname;
+    @Column(nullable = false, length = 50)
+    private String company;
+    @Column(nullable = false, length = 50)
+    private String position;
+    @Column(nullable = false, length = 50)
+    private String email;
+    @Column(nullable = false, length = 50)
+    private double salary;
 }
